a899414f6fcc51acc2c6da5b40da8784a58b6226
Vivian Li <vdl21@cam.ac.uk>
2023-12-27 17:02:51 +0000

    initialized emulators with a basic GPy GP model

--- src/emulator.py
+++ src/emulator.py
@@ -2,11 +2,18 @@ from typing import Tuple
 
 import numpy as np
 
+from GPy.models import GPRegression
 from emukit.core.interfaces import IModel, IModelWithNoise
 
 class GeneticDriftModel(IModel, IModelWithNoise):
-    def __init__(self):
-        pass
+    def __init__(self, X: np.ndarray, Y: np.ndarray):
+        """Initializes a GP model using GPy with an RBF kernel (variance 1, lengthscale 1) and Gaussian likelihood (noise variance 1)
+
+        Args:
+            X (np.ndarray): array of shape (n_points x n_inputs) of initial points
+            Y (np.ndarray): array of shape (n_points x n_outputs) of initial points
+        """
+        self.model = GPRegression(X, Y)
     
     def predict(self, X: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         """
@@ -15,7 +22,7 @@ class GeneticDriftModel(IModel, IModelWithNoise):
         :param X: array of shape (n_points x n_inputs) of points to run prediction for
         :return: Tuple of mean and variance which are 2d arrays of shape (n_points x n_outputs)
         """
-        raise NotImplementedError
+        return self.model.predict(X)
     
     def predict_noiseless(self, X: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         """
@@ -24,7 +31,7 @@ class GeneticDriftModel(IModel, IModelWithNoise):
         :param X: array of shape (n_points x n_inputs) of points to run prediction for
         :return: Tuple of mean and variance which are 2d arrays of shape (n_points x n_outputs)
         """
-        raise NotImplementedError
+        return self.model.predict(X, include_likelihood=False)
 
     def optimize(self) -> None:
         """
@@ -39,17 +46,26 @@ class GeneticDriftModel(IModel, IModelWithNoise):
         :param X: new points
         :param Y: function values at new points X
         """
-        raise NotImplementedError
+        self.model.set_XY(X, Y)
 
     @property
     def X(self):
-        raise NotImplementedError
+        return self.model.X
 
     @property
     def Y(self):
-        raise NotImplementedError
+        return self.model.Y
 
 class PopulationModel(IModel, IModelWithNoise):
+    def __init__(self, X: np.ndarray, Y: np.ndarray):
+        """Initializes a GP model using GPy with an RBF kernel (variance 1, lengthscale 1) and Gaussian likelihood (noise variance 1)
+
+        Args:
+            X (np.ndarray): array of shape (n_points x n_inputs) of initial points
+            Y (np.ndarray): array of shape (n_points x n_outputs) of initial points
+        """
+        self.model = GPRegression(X, Y)
+    
     def predict(self, X: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         """
         Predict mean and variance values for given points
@@ -57,7 +73,7 @@ class PopulationModel(IModel, IModelWithNoise):
         :param X: array of shape (n_points x n_inputs) of points to run prediction for
         :return: Tuple of mean and variance which are 2d arrays of shape (n_points x n_outputs)
         """
-        raise NotImplementedError
+        return self.model.predict(X)
     
     def predict_noiseless(self, X: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         """
@@ -66,7 +82,7 @@ class PopulationModel(IModel, IModelWithNoise):
         :param X: array of shape (n_points x n_inputs) of points to run prediction for
         :return: Tuple of mean and variance which are 2d arrays of shape (n_points x n_outputs)
         """
-        raise NotImplementedError
+        return self.model.predict(X, include_likelihood=False)
 
     def optimize(self) -> None:
         """
@@ -81,12 +97,12 @@ class PopulationModel(IModel, IModelWithNoise):
         :param X: new points
         :param Y: function values at new points X
         """
-        raise NotImplementedError
+        self.model.set_XY(X, Y)
 
     @property
     def X(self):
-        raise NotImplementedError
+        return self.model.X
 
     @property
     def Y(self):
-        raise NotImplementedError
+        return self.model.Y
