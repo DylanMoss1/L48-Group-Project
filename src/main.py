6ca902248f9ffb79f82d24bb321d0146f3fe04dd
Dylan Moss <dylan.moss2001@gmail.com>
2024-01-09 16:08:02 +0000

    Add population to debug info

--- src/main.py
+++ src/main.py
@@ -5,13 +5,13 @@ from pprint import pprint
 
 if __name__ == "__main__":
 
-    tiny_simulator = TinySimulator()
+    main_simulator = MainSimulator()
 
     mutation_rates = {
-        "size": 1,
-        "speed": 1,
-        "vision": 1,
-        "aggression": 1
+        "size": 0,
+        "speed": 0,
+        "vision": 0,
+        "aggression": 0,
     }
 
     # mutation_start_point = {
@@ -24,12 +24,13 @@ if __name__ == "__main__":
     # days_survived, log = main_simulator.run(mutation_rates, debug_info=DebugInfo(
     #     period=10, should_display_day=True, should_display_grid=False, should_display_traits=False), max_days=100)
 
-    days_survived, log = tiny_simulator.run(mutation_rates, debug_info=DebugInfo(
-        period=1, should_display_day=True, should_display_action=True, should_display_grid=True, should_display_traits=True), max_days=1)
+    # days_survived, log = tiny_simulator.run(mutation_rates, debug_info=DebugInfo(
+    #     period=1, should_display_day=True, should_display_action=True, should_display_grid=True, should_display_traits=True), max_days=1)
 
-    # days_survived, log = main_simulator.run(mutation_rates, max_days=1000)
+    days_survived, log = main_simulator.run(mutation_rates, debug_info=DebugInfo(
+        period=10, should_display_day=True, should_display_population=True), max_days=1000)
 
     print(days_survived)
 
-    for log_item in log:
-        print(log_item)
+    # for log_item in log:
+    #     print(log_item)
