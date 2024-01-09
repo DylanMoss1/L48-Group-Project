26cf6fceba34abc8d7356043ffe1779fd4894a6e
Dylan Moss <dylan.moss2001@gmail.com>
2024-01-09 21:44:45 +0000

    Changed base size to allow for reproduction

--- src/world.py
+++ src/world.py
@@ -832,11 +832,11 @@ class World:
         """
 
         reproduction_threshold = constants.REPRODUCTION_THRESHOLD
-        initial_energy = constants.INITIAL_ENERGY
         
         for row in self.grid:
             for location in row:
                 for species in location.species_list:
+
                     if species.energy >= reproduction_threshold:
                         species.energy = species.energy - reproduction_threshold / 2
 
@@ -847,11 +847,8 @@ class World:
                         child_traits = Species.get_child_traits(
                             parent_traits, self.mutation_rates)
 
-                        print("reached threshold")
-
                         # Generate a child from these trait values
                         if child_traits is not None:
-                            print("Added child")
                             location.add_species(Species(
                             size=child_traits["size"], speed=child_traits["speed"], vision=child_traits["vision"], aggression=child_traits["aggression"], energy=reproduction_threshold / 2))
                         
